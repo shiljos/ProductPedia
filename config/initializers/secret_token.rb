592ae5391e6e5a143be4832b52667bcf824801d4
shiljos <shiljos@gmail.com>
2014-03-11 10:36:20 +0100

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProductPedia::Application.config.secret_key_base = 'bb40b0033f7eb830ac3a05c5220823cb2189f56db7a5c709215cf745dc086121df48e26473d6157980ea4a35ef6e55ef36459c2cc61506287a3097200871b6f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProductPedia::Application.config.session_store :cookie_store, key: '_ProductPedia_session'
