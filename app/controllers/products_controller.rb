190112f30562bfba2ab250376e8a4b25e127a843
shiljos <shiljos@gmail.com>
2014-03-12 16:06:48 +0100

    defined product model and initial test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  skip_before_filter :authenticate_user!, only: :index
+
   def index
+  	@products = Product.all
   end
 end
--- spec/integration/view_products_spec.rb
+++ spec/integration/view_products_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 feature "Products list" do
   scenario "allows user to see all products" do
     visit products_path
+    expect(page).to have_css 'h1', text: 'Products'
+    #expect(page).to have_selector('ul')
+    #expect(page).to have_css 'li',  
   end
+
 end
