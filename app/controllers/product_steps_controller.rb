59221ee7a5647bd01ae71cfb8561a26aed8ff9ee
shiljos <shiljos@gmail.com>
2014-03-27 16:37:13 +0100

    create product functional

--- app/controllers/product_steps_controller.rb
+++ app/controllers/product_steps_controller.rb
@@ -6,11 +6,17 @@ class ProductStepsController < ApplicationController
 	def show
     @product = Product.find(params[:p])
     @prod_nuts = @product.product_nuts
-    render_wizard
+    session[:new_product]=params[:p]
+    render_wizard 
 	end
 
   def update
     ProductNut.update(params[:nutrients].keys, params[:nutrients].values)
-    redirect_to product_path(:p)
+    redirect_to_finish_wizard
+  end
+
+
+  def finish_wizard_path
+    product_path(session[:new_product])
   end
 end
