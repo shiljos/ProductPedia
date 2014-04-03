6e3c4687b371d977e2e80ffff081a3b8e2b67fa2
shiljos <shiljos@gmail.com>
2014-04-03 15:50:06 +0200

    Add new assets precompile locally task

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
 
 set(:config_files, %w(
   nginx_unicorn
